dcf624fc5232b7376c3f9efcbe59064caeb28201
Peter Buki <peter.buki@ericsson.com>
2018-11-17 17:52:05 +0100

    Refactored gethost.sh

--- src/script/gethost.sh
+++ src/script/gethost.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 SERVICE_PROTOCOL=http
 SERVICE_HOST=localhost
@@ -7,4 +7,7 @@ SERVICE_PATH=/findByHostname
 
 URL=${SERVICE_PROTOCOL}://${SERVICE_HOST}:${SERVICE_PORT}${SERVICE_PATH}
 
-curl -s -o  - ${URL}/?hostname=`hostname`
+PARAMS="hostname=`hostname`&columns="${COLUMNS}
+
+#echo ${URL}?${PARAMS}
+curl -s -o - ${URL}?${PARAMS}
